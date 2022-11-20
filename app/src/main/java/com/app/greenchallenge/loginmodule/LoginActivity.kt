c63bfa577e1724283119808d472c334886cf5b96
Nicolas Gil Villa <nggilv@ufpso.edu.co>
2022-11-20 14:34:38 -0500

    Refactor in packages

--- app/src/main/java/com/app/greenchallenge/loginmodule/LoginActivity.kt
+++ app/src/main/java/com/app/greenchallenge/loginmodule/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.app.greenchallenge
+package com.app.greenchallenge.loginmodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.greenchallenge.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
